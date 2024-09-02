005af9e284f5ceec50e8bd83da30ccea93e525cf
ShampooShrek <luizgsiewerdt@gmail.com>
2024-09-02 14:22:21 -0300

    Add case sensitive to query parameter in getCustomerMeasures

--- src/api/controllers/CustomerController.ts
+++ src/api/controllers/CustomerController.ts
@@ -6,7 +6,7 @@ class CustomerController {
   async getCustomerMeasures(req: Request, res: Response, next: NextFunction) {
     try {
       const measure_type: MeasureTypes | null =
-        (req.query.measure_type as keyof typeof MeasureTypes) ?? null
+        (req.query.measure_type?.toString().toUpperCase() as keyof typeof MeasureTypes) ?? null
       const response = await CustomerService.getCustomerMeasures(req.params.code, measure_type)
       res.status(200).json(response)
     } catch (err) {
